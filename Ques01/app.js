e3ce20dbeab8e23c1c86eb6ab90fa847114371a1
Aprajita Kashyap <kashyapaprajita2@gmail.com>
2025-09-06 23:19:36 +0530

    added Experiment-02

--- Ques01/app.js
+++
@@ -1,9 +0,0 @@
-// const myModule = require('./module');
-// console.log(myModule.greet('World'));
-// console.log(myModule.student);
-// console.log(myModule.greet("Aprajita"));
-
-// another way to import the module
-import { student, greet } from './module.js';
-console.log(greet('Aprajita'));
-console.log(student);
